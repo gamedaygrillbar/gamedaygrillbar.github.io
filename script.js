a35e277f3ae68c74283209a1df873cf3a5d37940
Dawid <dawidwas155@gmail.com>
2023-12-05 15:16:53 -0600

    Co-authored-by: jaimedhekar <jaimedhekar@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ document.getElementById("orderButton").addEventListener("click", function(){
     window.location.href = "menu.html"
 });
 
+// Media links
+document.getElementById("facebook").addEventListener("click", function(){
+    window.location.href = "https://www.facebook.com/"
+});
 
+document.getElementById("instagram").addEventListener("click", function(){
+    window.location.href = "https://www.instagram.com/"
+});
 
+document.getElementById("twitter").addEventListener("click", function(){
+    window.location.href = "https://www.twitter.com/"
+});
+
+document.getElementById("youtube").addEventListener("click", function(){
+    window.location.href = "https://www.youtube.com/"
+});
