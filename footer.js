87e2ec4ba547c34ff5d58a45ff8b69d69e3909d0
Dawid <dawidwas155@gmail.com>
2023-12-09 20:52:12 -0600

    Co-authored-by: Sean Thomas <IntegrityT18@users.noreply.github.com>

--- footer.js
+++ footer.js
@@ -1,27 +1,27 @@
-let elementsList = document.getElementsByClassName("bi-facebook")
+let elementsList = document.getElementsByClassName("bi-tiktok")
     for(let i = 0; i < elementsList.length; i++){
         elementsList[i].addEventListener("click", function(){
-            window.open("https://www.facebook.com/", "_blank")
+            window.open("https://www.tiktok.com/@gamedaygrillbar", "_blank")
         })
     }
 
-elementsList = document.getElementsByClassName("bi-instagram")
+elementsList = document.getElementsByClassName("bi-pinterest")
 for(let i = 0; i < elementsList.length; i++){
     elementsList[i].addEventListener("click", function(){
-        window.open("https://www.instagram.com/", "_blank")
+        window.open("https://www.pinterest.com/gamedaygrillbar/", "_blank")
     })
 }
 
 elementsList = document.getElementsByClassName("bi-twitter-x")
 for(let i = 0; i < elementsList.length; i++){
     elementsList[i].addEventListener("click", function(){
-        window.open("https://www.twitter.com/", "_blank")
+        window.open("https://twitter.com/gameday_grill", "_blank")
     })
 }
 
 elementsList = document.getElementsByClassName("bi-youtube")
 for(let i = 0; i < elementsList.length; i++){
     elementsList[i].addEventListener("click", function(){
-        window.open("https://www.youtube.com/", "_blank")
+        window.open("https://www.youtube.com/channel/UC0_Ci3sQW51Uim7HnD5RFcg", "_blank")
     })
 }
